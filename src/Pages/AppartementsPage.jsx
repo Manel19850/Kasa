a0697f47c2fd04061af9701b5d5c7088db5653a4
Manel Jouini <manl.j@hotmail.fr>
2023-07-06 12:07:23 +0200

    Modif de la page AppartementContainer

--- src/Pages/AppartementsPage.jsx
+++ src/Pages/AppartementsPage.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import "./AppartementsPage.css";
-import AppartementDescription from '../Composants/Appartementdescription';
 import AppartementBanner from '../Composants/AppartementBanner';
 import AppartementContainer from '../Composants/AppartementContainer';
+
+;
 
 function AppartementsPage (){
 
 return(
 <div className='AppartementsPages'>
-        <AppartementBanner/>
+        <AppartementBanner/>  
+        <AppartementContainer/>        
+</div>
 
-        <div className='ap-container'>
-            <AppartementContainer/>
-        </div>
-        
-                <div className='Ap-deroulante'>
-                    <AppartementDescription/>
-                    <AppartementDescription/>
-                </div>
-</div>)
+       
+
+
+
+
+)
 }
 
 export default AppartementsPage
