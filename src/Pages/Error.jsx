6a8e600f1c06f4a6330a073c1631f4e79b556a99
Manel Jouini <manl.j@hotmail.fr>
2023-07-08 12:09:30 +0200

    Création Page Error

---
+++ src/Pages/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Error.css';
+
+function Error() {
+    return (
+        <div className='error'>
+            <h1 className="error-message">404</h1>
+            <h2 className="error-message-subtitle">Oups! La page que vous demandez n'existe pas.</h2>
+            <h3 className='error-retour'>Retourner à la page d'accueil</h3>
+        </div>
+    );
+}
+
+export default Error;
--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,10 @@ import './index.css';
 import Accueil from './Pages/Accueil';
 import AppartementsPage from './Pages/AppartementsPage';
 import About from './Pages/About';
-import { BrowserRouter,Outlet,Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Footer from './Structure de Page/Footer';
 import Navbar from './Structure de Page/Navbar';
-import Banner from './Structure de Page/Banner';
+import Error from './Pages/Error';
 
 
 const StructurePage=()=>{
@@ -20,15 +20,11 @@ return <>
 
 export const Router = createBrowserRouter([
 { element: <StructurePage/>,
-errorElement: (
-  <>
-  <Navbar/>
-    <h1 className="error-message">404</h1>
-    <h2 className="error-message-subtitle">Oups! La page que vous demandez n'existe pas.</h2>
-    <h3 className='error-retour'> Retourner à la page d'accueil</h3>
-    <Footer/>
-  </>
-),
+errorElement: (<>
+                  <Navbar/>
+                  <Error/>
+              </>),
+
   children: [
 
     {path:"/",
